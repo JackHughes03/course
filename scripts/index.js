6067fe5eba82473a33a407b9dcd4f81aead674f1
JackHughes03 <101028724+JackHughes03@users.noreply.github.com>
2023-03-01 22:07:13 +0000

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,30 @@ video.autoplay = true;
 video.loop = true;
 video.muted = true;
 video.play();
+
+function darkmodeclicked() {
+    var html = document.getElementsByTagName("html")[0];
+    
+    var darkmode = document.getElementById("darkmode");
+
+    if (darkmode.src.match("images/darkmode.png")) {
+        darkmode.src = "images/darkmodelight.png";
+
+        if (html.style.filter == "invert(100%)") {
+            html.style.filter = "invert(0%)";
+        }
+        else {
+            html.style.filter = "invert(100%)";
+        }
+    }
+    else {
+        darkmode.src = "images/darkmodelight.png";
+
+        if (html.style.filter == "invert(100%)") {
+            html.style.filter = "invert(0%)";
+        }
+        else {
+            html.style.filter = "invert(100%)";
+        }
+    }
+}
