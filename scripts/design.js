7b0a774767e2d21d9fc472a15298cb96a0f1d835
JackHughes03 <101028724+JackHughes03@users.noreply.github.com>
2023-03-13 15:39:31 +0000

    Add files via upload

--- scripts/design.js
+++ scripts/design.js
@@ -1,18 +1,29 @@
 var cssjsnav = document.getElementById("cssjsnav");
-var csslinknav = document.querySelector(".csslinknav");
+var csslinknav = document.getElementsByClassName("csslinknav");
+var mainvideo = document.getElementById("mainvideo");
+var titlecourses = document.getElementById("titlecourses");
+cssjsnav.style.transition = "0.2s";
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {   
     if (window.scrollY === 0) {
         cssjsnav.style.backgroundColor = "transparent";
+        mainvideo.style.filter = "blur(0px)";
+        titlecourses.style.backgroundColor = "#b6b6b64f";
         
-        csslinknav.style.color = "black";
-        csslinknav.style.backgroundColor = "rgba(255, 255, 255, 0.564)";
-        csslinknav.style.padding = "10px 15px";
+        for (var i = 0; i < csslinknav.length; i++) {
+            csslinknav[i].style.color = "black";
+            csslinknav[i].style.backgroundColor = "rgba(255, 255, 255, 0.564)";
+            csslinknav[i].style.padding = "10px 15px";
+        }
     } else {
         cssjsnav.style.backgroundColor = "black";
+        mainvideo.style.filter = "blur(5px)";
+        titlecourses.style.backgroundColor = "transparent";
         
-        csslinknav.style.color = "white";
-        csslinknav.style.backgroundColor = "transparent";
-        csslinknav.style.padding = "0px";
+        for (var i = 0; i < csslinknav.length; i++) {
+            csslinknav[i].style.color = "white";
+            csslinknav[i].style.backgroundColor = "transparent";
+            csslinknav[i].style.padding = "0px";
+        }
     }
 });
